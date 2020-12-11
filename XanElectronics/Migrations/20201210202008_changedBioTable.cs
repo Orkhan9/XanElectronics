f45c4c434efba52d1b8f1d7b3afca871107b6656
Orkhan9 <orkhanmm@code.edu.az>
2020-12-11 13:38:41 +0400

    added subsribe

---
+++ XanElectronics/Migrations/20201210202008_changedBioTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XanElectronics.Migrations
+{
+    public partial class changedBioTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Bios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Bios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Bios");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Bios");
+        }
+    }
+}
---
+++ XanElectronics/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XanElectronics.Dal;
+using XanElectronics.Models;
+
+namespace XanElectronics.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly DataContext _context;
+        public HeaderViewComponent(DataContext context, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.Fullname = "";
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.Fullname = user.Fullname;
+            }
+
+            Bio model = _context.Bios.FirstOrDefault();
+            return View(await Task.FromResult(model));
+        }
+    }
+}
