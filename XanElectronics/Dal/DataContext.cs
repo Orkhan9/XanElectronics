0c8171b36494d9673fed843c76808bf67e1675f0
Orkhan9 <orkhanmm@code.edu.az>
2020-12-11 13:14:43 +0400

    added subsribe

---
+++ XanElectronics/Dal/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XanElectronics.Models;
+
+namespace XanElectronics.Dal
+{
+    public class DataContext :IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Bio> Bios { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ XanElectronics/ViewModels/RegisterVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XanElectronics.ViewModels
+{
+    public class RegisterVm
+    {
+        [Required, MaxLength(200)]
+        public string FullName { get; set; }
+        [Required, MaxLength(100)]
+        public string UserName { get; set; }
+        [Required, EmailAddress, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
