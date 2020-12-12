660e4b098afdc12339d2f24c88ed73113644be99
Orkhan9 <orkhanmm@code.edu.az>
2020-12-12 10:10:23 +0400

    added categoryCrud

---
+++ XanElectronics/ViewModels/CategoryCreateVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XanElectronics.ViewModels
+{
+    public class CategoryCreateVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [NotMapped,Required]
+        public IFormFile Image { get; set; }
+    }
+}
